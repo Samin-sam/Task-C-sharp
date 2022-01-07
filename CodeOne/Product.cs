fa5b50802a168a24280fa97361ebde3d37869e70
Kazi Samin Mahmud <saminsam362@gmail.com>
2022-01-08 01:25:15 +0600

    2nd

---
+++ CodeOne/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeOne
+{
+    class Product
+    {
+        private ushort id;
+        private string name;
+        private ulong price;
+
+        public ushort Getid()
+        {
+            return this.id;
+        }
+        public String Getname()
+        {
+            return this.name;
+        }
+
+        public ulong Getprice()
+        {
+            return this.price;
+        }
+
+        public void setid(ushort id)
+        {
+            this.id = id;
+        }
+        public void setname(string name)
+        {
+            this.name = name;
+        }
+        public void setprice(ulong price)
+        {
+            this.price = price;
+        }
+       
+        public Product()
+        {
+
+        }
+        public Product(ushort id, string name, ulong price)
+        {
+            this.setid(id);
+            this.setname(name);
+            this.setprice(price);
+        }
+
+        public void ShowProductInfo()
+        {
+            Console.WriteLine("Product Id:{0} ", this.id);
+            Console.WriteLine("Product Name:{0} ", this.name);
+            Console.WriteLine("Product price:{0} ", this.price);
+        }
+    }
+}
---
+++ CodeOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product productOne = new Product();
+            productOne.setid = 1000;
+            productOne.setname = "laptop";
+            productOne.setprice = 50000;
+
+            productone.ShowProductInfo();
+
+            Product productTwo = new Product(1000, "desktop", 40000);
+            productTwo.ShowProductInfo;
+
+        }
+     
+    }
+}
